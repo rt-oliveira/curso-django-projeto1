53de2ff800a3877c24a8b08c1efd2e7512764f00
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-05-27 11:33:07 -0300

    Uso de variáveis no HTML, para renderização dinâmica.

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', {
+        'name': 'Rafael',
+    })
 
 def contato(request):
     return HttpResponse('Contato')
