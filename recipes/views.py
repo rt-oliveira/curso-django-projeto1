62c184397e6e15c32587693b58e3abb39c29aa3b
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-05-27 10:52:46 -0300

    Modularizando o código da aplicação que será feita.

---
+++ recipes/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse('Home 1')
+
+def contato(request):
+    return HttpResponse('Contato')
+
+def sobre(request):
+    return HttpResponse('Sobre')
