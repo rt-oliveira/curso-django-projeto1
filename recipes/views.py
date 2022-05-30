2a7a3c3f1115f70cc84b19ee75fe0c1be4dc80b5
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-05-30 10:52:29 -0300

    Separando head do resto da página e estilizando-o.

--- recipes/views.py
+++ recipes/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html', {
+    return render(request, 'recipes/pages/home.html', {
         'name': 'Rafael',
     })
