409e39b7b909cf894d269d43e81e1d1dbac29a6c
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-06-02 10:30:28 -0300

    Primeiro contato com URLs com parâmetros.

--- recipes/views.py
+++ recipes/views.py
@@ -6,3 +6,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html', {
         'name': 'Rafael',
     })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', {
+        'name': 'Rafael',
+    })
