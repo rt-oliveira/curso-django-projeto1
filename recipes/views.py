be06d179004151340ef576f18537ad3dd1076407
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-06-02 13:15:03 -0300

    Injetando conteúdo fake para testar o site, e ajustando altura dos cards.

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/pages/home.html', {
-        'name': 'Rafael',
+    return render(request, 'recipes/pages/home.html', context = {
+        'recipes':  [make_recipe() for _ in range(10)]
     })
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', {
-        'name': 'Rafael',
+    return render(request, 'recipes/pages/recipe-view.html', context = {
+        'recipe':   make_recipe()
     })
