3547a1c1c6d804721b96f4dd0d077d72519eb17d
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-06-02 10:46:13 -0300

    Começando a criar view própria de detalhe de recipe.

--- recipes/views.py
+++ recipes/views.py
@@ -8,6 +8,6 @@ def home(request):
     })
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/home.html', {
+    return render(request, 'recipes/pages/recipe-view.html', {
         'name': 'Rafael',
     })
