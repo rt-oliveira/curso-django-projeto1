0e8d32ee0bc3189ebdbc664d2304e5d36663833a
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-06-22 13:40:58 -0300

    Testando Django Reverse com kwargs.

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 # recipes:recipe
@@ -6,8 +7,8 @@ app_name = 'recipes'
 
 urlpatterns = [
     # /
-    path('', views.home, name = "home"),
-    path('recipes/category/<int:category_id>/', views.category, name = "category"),
+    path('', views.home, name="home"),
+    path('recipes/category/<int:category_id>/', views.category, name="category"),  # noqa: E501
     # /recipe
-    path('recipes/<int:id>/', views.recipe, name = "recipe"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
