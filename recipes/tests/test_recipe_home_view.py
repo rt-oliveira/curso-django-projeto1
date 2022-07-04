facda96fe9bc63a211cc3a11ac04dcf45376a0b6
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-07-04 10:32:19 -0300

    Separando os testes de view em classes próprias.

---
+++ recipes/tests/test_recipe_home_view.py
@@ -0,0 +1,48 @@
+from unittest import skip
+
+from django.urls import resolve, reverse
+from recipes import views
+
+from .teste_recipe_base import RecipeTestBase
+
+
+class RecipeHomeViewTest(RecipeTestBase):
+
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_home_view_return_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    @skip('WIP')
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(
+            'No recipes found here :( .',
+            response.content.decode('utf-8')
+        )
+
+    def test_recipe_home_template_loads_recipes(self):
+        self.make_recipe()
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+
+        self.assertIn('Recipe Title', content)
+        self.assertEqual(len(response_context_recipes), 1)
+
+    def test_recipe_home_template_dont_load_recipe_not_published(self):
+        self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:home'))
+
+        self.assertIn(
+            'No recipes found here :( .',
+            response.content.decode('utf-8')
+        )
