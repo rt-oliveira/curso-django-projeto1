9f2f97928a9d5421f885a6c4aab8e065976f1544
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-07-08 11:53:15 -0300

    Adicionando flash messages.

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
@@ -10,6 +11,8 @@ def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
     ).order_by('-id')
+
+    messages.success(request, 'Epa, você foi pesquisar algo que eu vi.')
 
     page_obj, pagination_range = make_pagination(
         request, recipes, RECIPES_PER_PAGE)
