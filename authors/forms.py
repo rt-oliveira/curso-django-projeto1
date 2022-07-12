30b0922ed9ae0fe73a3c922a92e8626840db3c0a
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-07-12 10:37:36 -0300

    Customizando os elementos do formulário

--- authors/forms.py
+++ authors/forms.py
@@ -12,3 +12,31 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password'
         ]
+        # exclude = [
+        #     'first_name'
+        # ]
+        labels = {
+            "username": "Username",
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'email': 'E-mail',
+            'password': 'Password'
+        }
+        help_texts = {
+            'email': 'The e-mail must be valid!'
+        }
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty!',
+                'invalid': 'This field is invalid!'
+            }
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Your username here',
+                "class": 'input text-input'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            })
+        }
