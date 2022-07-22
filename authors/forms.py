1ba7327d0b822b6aaf4d757c3bb425118d42c7c2
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-07-22 15:43:32 -0300

    Validação com validators.

--- authors/forms.py
+++ authors/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
@@ -10,6 +12,18 @@ def add_attr(field, attr_name, attr_new_val):
 
 def add_placeholder(field, placeholder_val):
     add_attr(field, 'placeholder', placeholder_val)
+
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError((
+            'Password must have at least one uppercase letter, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        ),
+            code='Invalid')
 
 
 class RegisterForm(forms.ModelForm):
@@ -32,7 +46,8 @@ class RegisterForm(forms.ModelForm):
             'Password must have at least one uppercase letter, '
             'one lowercase letter and one number. The length should be '
             'at least 8 characters.'
-        )
+        ),
+        validators=[strong_password]
     )
 
     password2 = forms.CharField(
@@ -82,7 +97,7 @@ class RegisterForm(forms.ModelForm):
     def clean_password(self):
         data = self.cleaned_data.get('password')
 
-        if 'atencao' in data:
+        if 'atenção' in data:
             raise ValidationError(
                 'Não digite "atenção" no campo password',
                 code='invalid',
