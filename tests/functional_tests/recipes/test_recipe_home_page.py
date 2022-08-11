be634db745de6a93b6da426bffec60c697d31c14
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-08-11 11:32:14 -0300

    Automatizando testes

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 from selenium.webdriver.common.by import By
 
@@ -7,6 +9,7 @@ from .base import RecipeBaseFunctionalTest
 @pytest.mark.funcional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
 
+    @patch('recipes.views.RECIPES_PER_PAGE', new=3)
     def test_recipe_home_page_without_recipes_not_found_message(self):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
