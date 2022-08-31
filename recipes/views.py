42997f5deb5ac3fb08f28a2abdd8deb7e16888f5
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-08-31 13:41:26 -0300

    Corrigindo testes.

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,6 @@
 from django.db.models import Q
 from django.http import Http404
-from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 from utils.pagination import RECIPES_PER_PAGE, make_pagination
 
@@ -47,11 +47,14 @@ class RecipeListViewCategory(RecipeListViewBase):
             is_published=True
         )
 
+        if not qs:
+            raise Http404()
+
         return qs
 
     def get_context_data(self, *args, **kwargs):
         ctx = super().get_context_data(*args, **kwargs)
-        category_name = ctx['recipes'][0].category.name
+        category_name = ctx.get('recipes')[0].category.name
         ctx.update({
             'title':    f'{category_name} - Category | '
         })
@@ -64,6 +67,10 @@ class RecipeListViewSearch(RecipeListViewBase):
 
     def get_queryset(self, *args, **kwargs):
         search_term = self.request.GET.get('q', '')
+
+        if not search_term:
+            raise Http404()
+
         qs = super().get_queryset(*args, **kwargs)
 
         qs = qs.filter(
@@ -89,38 +96,6 @@ class RecipeListViewSearch(RecipeListViewBase):
         return ctx
 
 
-def home(request):
-    recipes = Recipe.objects.filter(
-        is_published=True
-    ).order_by('-id')
-
-    page_obj, pagination_range = make_pagination(
-        request, recipes, RECIPES_PER_PAGE)
-
-    return render(request, 'recipes/pages/home.html', context={
-        'recipes':  page_obj,
-        'pagination_range': pagination_range
-    })
-
-
-def category(request, category_id):
-    recipes = get_list_or_404(
-        Recipe.objects.filter(
-            category__id=category_id,
-            is_published=True
-        ).order_by('-id')
-    )
-
-    page_obj, pagination_range = make_pagination(
-        request, recipes, RECIPES_PER_PAGE)
-
-    return render(request, 'recipes/pages/category.html', context={
-        'recipes':  page_obj,
-        'pagination_range': pagination_range,
-        'title':    f'{recipes[0].category.name} - Category | '
-    })
-
-
 def recipe(request, id):
     recipe = get_object_or_404(
         Recipe,
@@ -131,30 +106,4 @@ def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe':           recipe,
         'is_detail_page':   True
-    })
-
-
-def search(request):
-    search_term = request.GET.get('q', '').strip()
-
-    if not search_term:
-        raise Http404
-
-    recipes = Recipe.objects.filter(
-        Q(
-            Q(title__icontains=search_term) |
-            Q(description__icontains=search_term)
-        ),
-        is_published=True
-    ).order_by('-id')
-
-    page_obj, pagination_range = make_pagination(
-        request, recipes, RECIPES_PER_PAGE)
-
-    return render(request, 'recipes/pages/search.html', {
-        'page_title':           f'Search for "{search_term}" |',
-        'search_term':          search_term,
-        'recipes':              page_obj,
-        'pagination_range':     pagination_range,
-        'additional_url_query': f'&q={search_term}'
     })
