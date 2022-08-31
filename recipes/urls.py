dd94d2df82d55c7eb42fd3673dc4236400fa2b9a
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-08-31 11:15:15 -0300

    CBV de search.

--- recipes/urls.py
+++ recipes/urls.py
@@ -8,8 +8,14 @@ app_name = 'recipes'
 urlpatterns = [
     # /
     path('', views.RecipeListViewHome.as_view(), name="home"),
-    path('recipes/search/', views.search, name="search"),
-    path('recipes/category/<int:category_id>/', views.RecipeListViewCategory.as_view(), name="category"),  # noqa: E501
+    path('recipes/search/',
+         views.RecipeListViewSearch.as_view(),
+         name="search"
+         ),
+    path('recipes/category/<int:category_id>/',
+         views.RecipeListViewCategory.as_view(),
+         name="category"
+         ),
     # /recipe
     path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
