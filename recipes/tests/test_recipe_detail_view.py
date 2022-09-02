5cfac193dcf8cdce9604509c27c0e60bfc40daf9
Rafael Teixeira <fael.teixeira@gmail.com>
2022-09-02 13:21:37 -0300

    Corrigindo os testes usando CBV.

--- recipes/tests/test_recipe_detail_view.py
+++ recipes/tests/test_recipe_detail_view.py
@@ -8,13 +8,13 @@ class RecipeDetailViewTest(RecipeTestBase):
 
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(
-            reverse('recipes:recipe', kwargs={'id': 1})
+            reverse('recipes:recipe', kwargs={'pk': 1})
         )
-        self.assertIs(view.func, views.recipe)
+        self.assertIs(view.func.view_class, views.RecipeDetail)
 
     def test_recipe_detail_view_return_404_if_no_recipes_found(self):
         response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id': 1000})
+            reverse('recipes:recipe', kwargs={'pk': 1000})
         )
         self.assertEqual(response.status_code, 404)
 
@@ -24,7 +24,7 @@ class RecipeDetailViewTest(RecipeTestBase):
         self.make_recipe(title=needed_title)
 
         response = self.client.get(reverse('recipes:recipe', kwargs={
-            'id': 1
+            'pk': 1
         }))
         content = response.content.decode('utf-8')
 
@@ -34,7 +34,7 @@ class RecipeDetailViewTest(RecipeTestBase):
         recipe = self.make_recipe(is_published=False)
 
         response = self.client.get(
-            reverse('recipes:recipe', kwargs={'id': recipe.id})
+            reverse('recipes:recipe', kwargs={'pk': recipe.id})
         )
 
         self.assertEqual(response.status_code, 404)
