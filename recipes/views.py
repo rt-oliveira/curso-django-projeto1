a851a6ac5e69e834ad001b3f2b2bd108ff5f88c6
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-09-28 11:53:56 -0300

    Exibindo tags.

--- recipes/views.py
+++ recipes/views.py
@@ -5,6 +5,7 @@ from django.forms.models import model_to_dict
 from django.http import Http404, JsonResponse
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
+from tag.models import Tag
 from utils.pagination import RECIPES_PER_PAGE, make_pagination
 
 from recipes.models import Recipe
@@ -22,6 +23,7 @@ class RecipeListViewBase(ListView):
             is_published=True
         )
         qs = qs.select_related('author', 'category')
+        qs = qs.prefetch_related('tags')
 
         return qs
 
@@ -182,3 +184,32 @@ def theory(request, *args, **kwargs):
         'recipes/pages/theory.html',
         context=context
     )
+
+
+class RecipeListViewTag(RecipeListViewBase):
+    template_name = 'recipes/pages/tag.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+
+        qs = qs.filter(
+            tags__slug=self.kwargs.get('slug', '')
+        )
+
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        page_title = Tag.objects.filter(
+            slug=self.kwargs.get('slug', '')).first()
+
+        if not page_title:
+            page_title = 'No recipes found'
+
+        page_title = f'{page_title} - Tag |'
+
+        ctx.update({
+            'page_title': page_title
+        })
+
+        return ctx
