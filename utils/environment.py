7ecd643ad1531af9fb769d582f4322e5ed0f121b
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-09-29 10:46:48 -0300

    Fix environment.py

--- utils/environment.py
+++ utils/environment.py
@@ -6,6 +6,6 @@ def get_env_variable(variable_name, default_value=''):
 
 
 def parse_comma_sep_str_to_list(comma_sep_str):
-    if not comma_sep_str or isinstance(comma_sep_str, str):
+    if not comma_sep_str or not isinstance(comma_sep_str, str):
         return []
     return [string.strip() for string in comma_sep_str.split(',') if string]
