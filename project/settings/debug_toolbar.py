394575983fbcc2002f597f5cf493f9db0f0b127a
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-09-29 11:11:20 -0300

    Organizando o settings.py em um pacote.

---
+++ project/settings/debug_toolbar.py
@@ -0,0 +1,11 @@
+from .installed_apps import INSTALLED_APPS
+from .middleswares import MIDDLEWARE
+
+INSTALLED_APPS += ['debug_toolbar', ]
+MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+] + MIDDLEWARE
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
