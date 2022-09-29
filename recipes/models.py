9fb8972cd08592dad9a59ac30f5eb6308ab620dd
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-09-29 14:00:08 -0300

    Validação no model.

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,8 @@
+from collections import defaultdict
+
 from django.contrib.auth.models import User
 from django.db import models
+from django.forms import ValidationError
 from django.urls import reverse
 from django.utils.text import slugify
 from tag.models import Tag
@@ -55,3 +58,18 @@ class Recipe(models.Model):
             self.slug = slug
 
         return super().save(*args, **kwargs)
+
+    def clean(self, *args, **kwargs):
+        error_messages = defaultdict(list)
+
+        recipe_from_db = Recipe.objects.filter(
+            title__iexact=self.title
+        ).first()
+
+        if recipe_from_db:
+            if recipe_from_db.pk != self.pk:
+                error_messages['title'].append(
+                    'Found recipes with the same title')
+
+        if error_messages:
+            raise ValidationError(error_messages)
