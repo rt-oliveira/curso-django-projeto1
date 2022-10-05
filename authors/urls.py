69374915325553f4a979a8ab06b7cc526815f2e3
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-10-05 15:37:14 -0300

    Criando template, url e view para profile.

--- authors/urls.py
+++ authors/urls.py
@@ -11,14 +11,13 @@ urlpatterns = [
     path('login/create/', views.login_create, name='login_create'),
     path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
-
     path('dashboard/recipe/<int:id>/edit/',
          views.DashboardRecipe.as_view(), name='dashboard_recipe_edit'),
-
     path('dashboard/recipe/new/', views.DashboardRecipe.as_view(),
          name='dashboard_recipe_new'),
-
     path('dashboard/recipe/delete/',
          views.DashboardRecipeDelete.as_view(),
          name='dashboard_recipe_delete'),
+    path('profile/<int:id>/',
+         views.ProfileView.as_view(), name='profile'),
 ]
