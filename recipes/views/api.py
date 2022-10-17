af5f32cac277fdb946db625999608093d9b99eef
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-10-17 11:05:54 -0300

    Tratando dados enviados à API.

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from tag.models import Tag
@@ -7,12 +8,21 @@ from ..models import Recipe
 from ..serializers import RecipeSerializer, TagSerializer
 
 
-@api_view()
+@api_view(http_method_names=['get', 'post'])
 def recipes_api_list(request):
-    recipes = Recipe.objects.get_published()[:10]
-    serializer = RecipeSerializer(instance=recipes, many=True,
-                                  context={'request': request})
-    return Response(serializer.data)
+    if request.method == 'GET':
+        recipes = Recipe.objects.get_published()[:10]
+        serializer = RecipeSerializer(instance=recipes, many=True,
+                                      context={'request': request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = RecipeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            serializer.validated_data,
+            status=status.HTTP_201_CREATED
+        )
 
 
 @api_view()
