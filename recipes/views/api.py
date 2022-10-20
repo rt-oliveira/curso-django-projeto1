57bbbc8b0336269e05fe3d9c739f9f595e10bef0
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-10-20 13:16:29 -0300

    Não usar nome de método HTTP em viewset.

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -18,7 +18,7 @@ class RecipeAPIV2ViewSet(ModelViewSet):
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIV2Pagination
 
-    def patch(self, request, *args, **kwargs):
+    def partial_update(self, request, *args, **kwargs):
         pk = kwargs.get('pk')
         recipe = self.get_queryset().filter(pk=pk).first()
         serializer = RecipeSerializer(
