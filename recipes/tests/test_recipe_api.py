12e977812b31130c9c0644fcb9bb0ab69fc87e57
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-11-07 10:19:55 -0300

    Primeiro teste unitário na API.

---
+++ recipes/tests/test_recipe_api.py
@@ -0,0 +1,14 @@
+from django.urls import reverse
+from recipes.tests.teste_recipe_base import RecipeMixin
+from rest_framework import test
+
+
+class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
+    def test_recipe_api_list_returns_status_code_200(self):
+        api_url = reverse('recipes:recipe-api-list')
+        response = self.client.get(api_url)
+
+        self.assertEqual(
+            response.status_code,
+            200
+        )
