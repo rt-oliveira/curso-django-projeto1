6ed29b43550fe2d6c27de09bf89bd5cc4cceb5c2
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-11-07 11:07:42 -0300

    Testando se receitas não publicadas não estão aparecendo.

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -6,9 +6,13 @@ from rest_framework import test
 
 
 class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
-    def test_recipe_api_list_returns_status_code_200(self):
+    def get_recipe_api_list(self):
         api_url = reverse('recipes:recipe-api-list')
         response = self.client.get(api_url)
+        return response
+
+    def test_recipe_api_list_returns_status_code_200(self):
+        response = self.get_recipe_api_list()
 
         self.assertEqual(
             response.status_code,
@@ -26,4 +30,16 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
         self.assertEqual(
             qtd_of_loaded_recipes,
             wanted_number_of_recipes
+        )
+
+    def test_recipe_api_list_do_not_shoe_not_published_recipes(self):
+        recipes = self.criar_recipes_em_lote(quantidade=2)
+        recipe_not_published = recipes[0]
+        recipe_not_published.is_published = False
+        recipe_not_published.save()
+
+        response = self.get_recipe_api_list()
+        self.assertEqual(
+            len(response.data.get('results')),
+            1
         )
