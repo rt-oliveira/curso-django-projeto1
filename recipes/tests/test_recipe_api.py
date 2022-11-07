666885fb15fdd39f9cc2059ad962fa6134149673
Rafael Teixeira de Oliveira <rafael.oliveira@unirio.br>
2022-11-07 10:31:26 -0300

    Olhando a resposta dada pela API.

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from django.urls import reverse
 from recipes.tests.teste_recipe_base import RecipeMixin
 from rest_framework import test
@@ -11,4 +13,17 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
         self.assertEqual(
             response.status_code,
             200
+        )
+
+    @patch('recipes.views.api.RecipeAPIV2Pagination.page_size', new=7)
+    def test_recipe_api_list_loads_correct_number_of_recipes(self):
+        wanted_number_of_recipes = 9
+        self.criar_recipes_em_lote(wanted_number_of_recipes)
+
+        response = self.client.get(reverse('recipes:recipe-api-list'))
+        qtd_of_loaded_recipes = len(response.data.get('results'))
+
+        self.assertEqual(
+            qtd_of_loaded_recipes,
+            wanted_number_of_recipes
         )
